fabb1ffc6c91902a8ce40057ed87f833bd9f0c65
Azake <mr.rysbaev@gmail.com>
2021-12-31 23:49:48 +0600

    trying to change the  C.Norris to users name, still have bugs

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const buttonEl = document.querySelector(".jokeBtn")
 const jokeText = document.querySelector(".jokeText")
 const image = document.querySelector("img")
+const anyName = document.queryCommandValue("#name")
 
+const anySurname = document.queryCommandValue("#surname")
 
 buttonEl.addEventListener("click", () => {
+    
     fetch("http://api.icndb.com/jokes/random") //method to bring data from server
     .then(response => response.json())
+    .then(newName = anyName.value)
     .then(data => {
-        jokeText.innerHTML = data.value.joke
+        const jokeBox = data.value.joke
+        result = jokeBox.replace('Chuck Norris', newName); 
+        console.log(newName)
+        jokeText.innerText = result
+
     })
     .catch(error => console.log(error, "this is error"))
 
